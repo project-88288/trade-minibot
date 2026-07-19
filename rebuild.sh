80f01ee82a8006ebad7ef6789c08456ea2f16269
Opzlabs <chaiya.88288@gmail.com>
2026-07-19 16:17:08 +0700

    Add GitHub Actions

--- rebuild.sh
+++ rebuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-docker build -t chaiya0899223232/ftrade-mini-bot:latest .
-docker push chaiya0899223232/ftrade-mini-bot:latest
+docker build -t 88288/ftrade-mini-bot:latest .
+docker push 88288/ftrade-mini-bot:latest
 
-echo "Successfully rebuilt and pushed chaiya0899223232/ftrade-mini-bot:latest"
+echo "Successfully rebuilt and pushed 88288/ftrade-mini-bot:latest"
